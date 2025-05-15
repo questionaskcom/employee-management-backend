107f959c7bc8c8579d2781143564a652861cfcbd
questionaskcom <questionask.com@gmail.com>
2025-05-15 22:06:31 +0530

    add changes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use Illuminate\Http\Request;
 
-Route::middleware('auth:sanctum')->get('/user', [AuthController::class, 'me']);
+
+// Route::middleware('auth:sanctum')->get('/user', [AuthController::class, 'me']);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
