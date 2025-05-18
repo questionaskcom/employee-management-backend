784287b3688a7dfbe780a4d79c08738d9607cf73
questionaskcom <questionask.com@gmail.com>
2025-05-18 21:40:02 +0530

    add changes

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,10 @@ return [
     'allowed_methods' => ['*'],
 
     // 'allowed_origins' => ['*'],
-
+'allowed_origins' => [
+        'http://localhost:3000',
+        'http://127.0.0.1:3000'
+    ],
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
