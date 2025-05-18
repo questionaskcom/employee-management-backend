b7cd5b3333b6a1f48d2e8ab5398c2a90f5926899
questionaskcom <questionask.com@gmail.com>
2025-05-18 14:06:06 +0530

    add changes

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('department_name');
+            $table->string('department');
             $table->string('phone');
             $table->rememberToken();
             $table->timestamps();
